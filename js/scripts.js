6e3702a9f2b9ee07d3520095c14eedd34cbb7cbf
Hashir <hashirbusinessinfo@gmail.com>
2024-03-30 05:44:05 +0500

    completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-const hamburgetMenu  = document.querySelector('.hamburger > img');
+const hamburgetMenu  = document.querySelector('.hamburger-menu-container > img');
 
 hamburgetMenu.addEventListener('click', () => {
     const nav = document.querySelector('.hamburger-menu');
-    nav.classList.toggle('hamburger-menu-display');
+    nav.classList.toggle('hamburger-menu-anzeige');
     }
 );
